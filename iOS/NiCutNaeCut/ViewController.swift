37212d5e6f7a3eafd6159af7fc80938a31e0068c
GJBBang <kzo12341@gmail.com>
2023-05-29 15:15:27 +0900

    NIC-2 feat: iOS 프로젝트 생성

---
+++ iOS/NiCutNaeCut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NiCutNaeCut
+//
+//  Created by BBangGiJin on 2023/05/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
